ecc5e21e62894be45a46737412dccea4a6436b23
Grzegorz Zając <gzajac11@gmail.com>
2021-02-11 21:54:56 +0100

    small changes

--- config.py
+++ config.py
@@ -27,11 +27,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
-    DB_HOST = os.environ.get('DB_HOST')
-    DB_PORT = os.environ.get('DB_PORT')
-    DB_NAME = os.environ.get('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 config = {
